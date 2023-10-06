670658e3f8b77785709376f73843249b93533baa
Saito Tsutomu <tsutomu7@hotmail.co.jp>
2023-10-07 06:33:32 +0900

    v0.1.1

--- knapsack/__init__.py
+++ knapsack/__init__.py
@@ -6,29 +6,31 @@ except ImportError:
     from backports.functools_lru_cache import lru_cache
 
 # see pyproject.toml
-__version__ = "0.0.6"
+__version__ = "0.1.1"
 __author__ = "Saito Tsutomu <tsutomu7@hotmail.co.jp>"
 
 
 class knapsack:
-    """
+    """Solving the knapsack problem.
+
     Maximize sum of selected weight.
     Sum of selected size is les than capacity.
     Algorithm: Dynamic Optimization
-    ----
-    import knapsack
-    size = [21, 11, 15, 9, 34, 25, 41, 52]
-    weight = [22, 12, 16, 10, 35, 26, 42, 53]
-    capacity = 100
-    knapsack.knapsack(size, weight).solve(capacity)
+
+    >>> import knapsack
+    >>> size = [21, 11, 15, 9, 34, 25, 41, 52]
+    >>> weight = [22, 12, 16, 10, 35, 26, 42, 53]
+    >>> capacity = 100
+    >>> knapsack.knapsack(size, weight).solve(capacity)
+    (105, [0, 1, 3, 4, 5])
     """
 
-    def __init__(self, size, weight):
+    def __init__(self, size: list[int], weight: list[int]):
         self.size = size
         self.weight = weight
 
     @lru_cache(maxsize=4096 * 4096)
-    def solve(self, cap, i=0):
+    def solve(self, cap: int, i: int = 0):
         if cap < 0:
             return -sum(self.weight), []
         if i == len(self.size):
